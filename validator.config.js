d9e4b8e32cf9716ac12416cdbd698e081102fe6f
Rauan Akylzhanov <akylzhanov.r@gmail.com>
2025-08-04 08:00:15 +0000

    save validator config

--- validator.config.js
+++ validator.config.js
@@ -2,13 +2,13 @@ module.exports = {
       apps : [{
         name   : 'validator_validator_process',
         script : 'python3',
-        cwd    : '/home/rataski/HappyAI-subnet',
+        cwd    : '/workspace/HappyAI-subnet',
         interpreter: 'none',
         min_uptime: '5m',
         max_restarts: '5',
-        args: ['-m', 'app.neurons.validator', '--netuid','1','--subtensor.network','finney','--wallet.name','crypto_wallet','--wallet.hotkey','validator1','--logging.debug','--blacklist.force_validator_permit','--axon.port','8091'],
+        args: ['-m', 'app.neurons.validator', '--netuid','103','--subtensor.network','finney','--wallet.name','ckli03','--wallet.hotkey','hkli03','--logging.debug','--blacklist.force_validator_permit','--axon.port','54619'],
         env: {
-          PYTHONPATH: '/home/rataski/HappyAI-subnet'
+          PYTHONPATH: '/workspace/HappyAI-subnet'
         }
       }]
     }
