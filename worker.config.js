b9b7e18abd4b687263e50a3ee1026c25c039d221
Rauan Akylzhanov <akylzhanov.r@gmail.com>
2025-08-04 08:00:07 +0000

    save worker config

--- worker.config.js
+++ worker.config.js
@@ -2,13 +2,13 @@ module.exports = {
       apps : [{
         name   : 'validator_worker_process',
         script : 'python3',
-        cwd    : '/home/rataski/HappyAI-subnet/worker',
+        cwd    : '/workspace/HappyAI-subnet/worker',
         interpreter: 'none',
         min_uptime: '5m',
         max_restarts: '5',
         args: ['-m', 'uvicorn', 'app.main:app', '--host', '0.0.0.0', '--port', '1235'],
         env: {
-          PYTHONPATH: '/home/rataski/HappyAI-subnet'
+          PYTHONPATH: '/workspace/HappyAI-subnet'
         }
       }]
     }
